6b8b11618fc3c80987228a3dd931b95b0ba4ba8f
omarqr0 <pleess30@gmail.com>
2022-04-19 03:29:50 +0300

    set the parameters for the api request

--- controller/server.js
+++ controller/server.js
@@ -1,34 +1,76 @@
-
 // setting up express.js
-const express = require('express');
+const express = require("express");
 const app = express();
+//const async = require('async');
 const port = process.env.PORT || 3000;
-const {Client} = require("@googlemaps/google-maps-services-js");
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-app.use(express.static('public'));
+const { Client } = require("@googlemaps/google-maps-services-js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+app.use(express.static("public"));
 app.use(express.json());
-require('dotenv').config();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send({express: 'Express is connected to react'}) 
+app.get("/", (req, res) => {
+  res.send({ express: "Express is connected to react" });
 });
 
-var lat=0, lon=0;
-app.post('/', (req,res) =>{
-  lat = req.body.lat;
-  lon = req.body.lon;
-  initGoogle(lat, lon);
+
+// getting the latitude a the longitude
+app.post("/", async (req, res) => {
+  const lat = req.body.lat;
+  const lon = req.body.lon;
+  const latlng = [lat, lon];
+  const result = await postLatlng(lat, lon);
+  initGoogle(lat, lon, "restaurant");
   res.json({
-    status: 'success',
+    status: "success",
     latitude: lat,
     longitude: lon,
   });
 });
 
+
+app.get("/restaurants", (req, res) => {
+  //const restaurantRes = initGoogle(lat,lon, "restaurant");
+  //console.log(restaurantRes);
+  //res.send(restaurantRes);
+});
+
+app.get("/cafes", (req, res) => {
+  const cafeRes = initGoogle(latlng[0], latlng[1], "cafe");
+  res.send(cafeRes);
+});
+
+app.get("/entertainment", (req, res) => {});
+
 app.listen(port, () => console.log(`Listening to port ${port}`));
 
-function initGoogle(lat, lon) {
-
+function initGoogle(lati, lon, reqType) {
+  var Latlng = [lati, lon];
+  const client = new Client({});
+  client
+    .placesNearby({
+      params: {
+        location: Latlng,
+        type: [reqType],
+        radius: 10000,
+        opennow: true,
+        key: process.env.MAPS_API,
+      },
+    })
+    .then((r) => {
+      const results = r.data.results[0].geometry.name
+      //console.log(results);
+      // results.forEach(element => {
+      //   console.log(element.);
+      // });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 
+async function postLatlng(lat, lon) {
+  const latlng= [lat, lon]
+}
 
